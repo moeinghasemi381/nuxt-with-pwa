8dc07c5bcd568f295aabf8d99549e0598c471eb5
moeinghasemi381 <moeinghasemi381@gmail.com>
2020-11-14 22:54:55 +0330

    changed nuxt.config.js file in order the project to get deployed on vercel. (for the third time)

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,22 +6,20 @@ export default {
     titleTemplate: '%s - nuxt-with-pwa',
     title: 'nuxt-with-pwa',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -66,6 +64,7 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    dir: 'public'
   },
 
   generate: {
